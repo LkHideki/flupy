27641f6962cb21e3db6795221a0ea6a4ccfac16f
LkHideki <lucas.hideki.07@gmail.com>
2024-03-06 10:45:14 -0300

    Novos métodos e Exceção descritiva

--- 01_prologo/01_modelo_de_dados/01_namedtuple.py
+++ 01_prologo/01_modelo_de_dados/01_namedtuple.py
@@ -1,22 +1,51 @@
 import collections
 from itertools import product
-from random import sample
+from random import sample, shuffle
+
+class ExtractionException(Exception):
+    def __init__(self, message="Extraction failed", errors=None) -> None:
+        super().__init__(message)
+        self.errors = errors
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 class FrenchDeck:
-    ranks = [str(n) for n in range(2,11)] + list('JQKA')
-    suits = 'spades diamonds clubs hearts'.split()
+    _ranks = [str(n) for n in range(2,11)] + list('JQKA')
+    _suits = 'spades diamonds clubs hearts'.split()
+    _cards = [Card(r, s) for r,s in product(_ranks, _suits)]
     
     def __init__(self) -> None:
-        self._cards = [Card(r, s) for r,s in product(self.ranks, self.suits)]
         self.cards = sample(self._cards, len(self._cards))
         
     def __len__(self) -> int:
-        return len(self._cards)
+        return len(self.cards)
     
     def __getitem__(self, position: int) -> Card:
         return self.cards[position]
+
+    def shuffle(self):
+        """
+        Shuffles the cards in the deck.
+        """
+        shuffle(self.cards)
+
+    def pick(self, number_of_cards: int = 1) -> list[Card]:
+        """
+        Picks a specified number of cards from the deck.
+
+        Args:
+            number_of_cards (int): The number of cards to pick from the deck. Defaults to 1.
+
+        Returns:
+            list[Card]: A list of picked cards.
+        """
+        extractions: list[Card] = []
+        if len(self) < number_of_cards:
+            raise ExtractionException(f"Tried to extract {number_of_cards} cards, but there are only {len(self)} cards remaining.")
+        
+        for _ in range(number_of_cards):
+            extractions.append(self.cards.pop())
+        return extractions
 
 
 if __name__ == "__main__":
