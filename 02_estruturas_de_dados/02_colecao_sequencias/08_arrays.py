ff75e33ab8453a282cfcde9190f3749d72da25cd
LkHideki <lucas.hideki.07@gmail.com>
2024-03-07 19:19:30 -0300

    separação das funções

--- 02_estruturas_de_dados/02_colecao_sequencias/08_arrays.py
+++ 02_estruturas_de_dados/02_colecao_sequencias/08_arrays.py
@@ -2,15 +2,20 @@ from array import array
 from os import path
 from random import random
 
-with open("arquivo_numeros", "wb") as arq:
-    my_array = array("d", [random() for _ in range(1_000_000 - 1)])
-    my_array.insert(1, 19)
-    my_array.tofile(arq)
+def crie():
+    with open("arquivo_numeros", "wb") as arq:
+        my_array = array("d", [random() for _ in range(1_000_000 - 1)])
+        my_array.insert(1, 19)
+        my_array.tofile(arq)
 
-if path.exists("arquivo_numeros"):
-    with open("arquivo_numeros", "rb") as arq:
-        arr = array("d")
-        arr.fromfile(arq, int(1e6))
-        print(*arr[:10], sep="\n")
+def leia():
+    if path.exists("arquivo_numeros"):
+        with open("arquivo_numeros", "rb") as arq:
+            arr = array("d")
+            arr.fromfile(arq, int(1e6))
+            print(*arr[:10], sep="\n")
 
-    print(*array("d", map(lambda m: round(10 * m, 2), arr[:10])))
+        print(*array("d", map(lambda m: round(10 * m, 2), arr[:10])))
+
+if __name__ == "__main__":
+    crie()
