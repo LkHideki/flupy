fafcebaf6c7e37eca06898acee045d35b9b06e00
LkHideki <lucas.hideki.07@gmail.com>
2024-03-08 09:59:16 -0300

    Um pequeno exercício sobre memoryview e bytearray

--- 02_estruturas_de_dados/02_colecao_sequencias/09_memoryviews.py
+++ 02_estruturas_de_dados/02_colecao_sequencias/09_memoryviews.py
@@ -7,11 +7,20 @@ sem criar cópias.
 from array import array
 
 arr = array("i", [78, 79, 80, 81])
-
 memv = memoryview(arr)
 
-k = memv[1:-1]
+fatia = memv[1:-1]
 
-k[1] = 1000
+fatia[1] = 1000
 
 print(arr)
+
+
+# outro exemplo (fora do livro)
+# Como "editar" uma string (obj imutável)
+nome = bytearray("lucas", 'utf-8') # Na verdade estamos mexendo num bytearray
+print("nome antes :", nome)
+
+mv = memoryview(nome)
+mv[2] = ord("C") # e editando o próprio
+print("nome depois:", nome)
