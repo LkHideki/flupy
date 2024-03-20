30970b4e8d22678655133b3737771c7a6935da49
LkHideki <lucas.hideki.07@gmail.com>
2024-03-20 10:02:36 -0300

    obtendo os parâmetros com a função signature

--- 03_funcoes/05_func_primeira_classe/04_obtendo_informacoes.py
+++ 03_funcoes/05_func_primeira_classe/04_obtendo_informacoes.py
@@ -7,8 +7,12 @@ se 04_obtendo_informacoes.py for o nome deste arquivo.
 """
 
 import bobo
-
+from inspect import signature
 
 @bobo.query("/")
 def hello(person):
     return f"Hello, {person}!"
+
+if __name__ == "__main__":
+    # Não vou comentar a discussão sobre os dunder defaults e code.
+    print(signature(print))
