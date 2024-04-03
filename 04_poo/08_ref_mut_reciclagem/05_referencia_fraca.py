0d7efbe9aca4b802b7d3ddcc8ac9c8ec814dae85
LkHideki <lucas.hideki.07@gmail.com>
2024-04-03 20:02:24 -0300

    só deixando mais apresentável

--- 04_poo/08_ref_mut_reciclagem/05_referencia_fraca.py
+++ 04_poo/08_ref_mut_reciclagem/05_referencia_fraca.py
@@ -3,9 +3,6 @@
 é uma referência, mas que não incrementa o contador de referências de um objeto (o referente).
 
 Então, uma referência fraca não impede um objeto de ser destruído.
-
-Exemplo de uso: cache.
-
 '''
 import weakref
 import gc
@@ -20,7 +17,7 @@ print("wr() ->", wr())
 
 print("wr() is None ->", wr() is None)
 
-print('Invocando o coletor...')
+print('\nInvocando o coletor...')
 del a
 gc.collect() # Invoca o coletor imediatamente
 
