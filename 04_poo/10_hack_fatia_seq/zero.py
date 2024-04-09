6980f4f6e0bbfd99f7eacfaca49d0e0eb62c66bf
LkHideki <lucas.hideki.07@gmail.com>
2024-04-09 16:15:26 -0300

    implementando o protocolo de sequência no vetor

--- 04_poo/10_hack_fatia_seq/zero.py
+++ 04_poo/10_hack_fatia_seq/zero.py
@@ -42,3 +42,47 @@ class Tomada1:
             typecode = chr(octets[0])
             memv = memoryview(octets[1:]).cast(typecode)
             return cls(memv)
+
+class Tomada2:
+    class Vetor:
+        typecode = "d"
+
+        def __init__(self, components) -> None:
+            self._components = array(self.typecode, components)
+
+        def __iter__(self):
+            return iter(self._components)
+
+        def __repr__(self) -> str:
+            components = reprlib.repr(self._components)
+            components = components[components.find("[") : -1]
+            return f"Vetor({components})"
+
+        def __str__(self) -> str:
+            return str(tuple(self))
+
+        def __bytes__(self) -> bytes:
+            tc = bytes([ord(self.typecode)])
+            comp = bytes(self._components)
+            return tc + comp
+
+        def __eq__(self, value) -> bool:
+            return tuple(self) == tuple(value)
+
+        def __abs__(self) -> float:
+            return math.sqrt(sum(x * x for x in self))
+
+        def __bool__(self) -> bool:
+            return bool(abs(self))
+
+        def __len__(self) -> int:
+            return len(self._components)
+
+        def __getitem__(self, i: int):
+            return self._components[i]
+
+        @classmethod
+        def frombytes(cls, octets):
+            typecode = chr(octets[0])
+            memv = memoryview(octets[1:]).cast(typecode)
+            return cls(memv)
