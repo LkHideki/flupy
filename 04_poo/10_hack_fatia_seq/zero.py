37ea9c916878d75ab652d7aa048087d2d1e71805
LkHideki <lucas.hideki.07@gmail.com>
2024-04-10 08:46:32 -0300

    inclusão do shortcut via dunder getattr

--- 04_poo/10_hack_fatia_seq/zero.py
+++ 04_poo/10_hack_fatia_seq/zero.py
@@ -93,3 +93,63 @@ class Tomada2:
             typecode = chr(octets[0])
             memv = memoryview(octets[1:]).cast(typecode)
             return cls(memv)
+
+class Tomada3:
+    class Vetor:
+        typecode = "d"
+        shortcuts = 'xyzw'
+
+        def __init__(self, components) -> None:
+            self._components = array(self.typecode, components)
+
+        def __iter__(self):
+            return iter(self._components)
+
+        def __repr__(self) -> str:
+            components = reprlib.repr(self._components)
+            components = components[components.find("[") : -1]
+            return f"Vetor({components})"
+
+        def __str__(self) -> str:
+            return str(tuple(self))
+
+        def __bytes__(self) -> bytes:
+            tc = bytes([ord(self.typecode)])
+            comp = bytes(self._components)
+            return tc + comp
+
+        def __eq__(self, value) -> bool:
+            return tuple(self) == tuple(value)
+
+        def __abs__(self) -> float:
+            return math.sqrt(sum(x * x for x in self))
+
+        def __bool__(self) -> bool:
+            return bool(abs(self))
+
+        def __len__(self) -> int:
+            return len(self._components)
+
+        def __getitem__(self, i: int):
+            cls = type(self)
+            if isinstance(i, slice):
+                return cls(self._components[i])
+            elif isinstance(i, numbers.Integral):
+                return self._components[i]
+            msg = f'{cls.__name__}: os índices devem ser números inteiros.'
+            raise TypeError(msg)
+
+        def __getattr__(self, attr: str):
+            cls = type(self)
+            if len(attr) == 1:
+                i = cls.shortcuts.find(attr)
+                if 0 <= i < len(self._components):
+                    return self._components[i]
+            msg = f"{cls.__name__} não tem atributo {attr}"
+            raise AttributeError(msg)
+
+        @classmethod
+        def frombytes(cls, octets):
+            typecode = chr(octets[0])
+            memv = memoryview(octets[1:]).cast(typecode)
+            return cls(memv)
