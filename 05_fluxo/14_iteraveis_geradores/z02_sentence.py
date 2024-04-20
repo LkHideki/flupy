ba8ecd9576f792aae2571a2c08c2151221b00f0d
LkHideki <lucas.hideki.07@gmail.com>
2024-04-20 13:45:22 -0300

    criação e exploração da classe Sentença

---
+++ 05_fluxo/14_iteraveis_geradores/z02_sentence.py
@@ -0,0 +1,19 @@
+import re, reprlib
+
+RE_WORD = re.compile(r'\w+')
+
+class Sentence:
+    def __init__(self, text: str) -> None:
+        self.text = text.strip()
+        self.words = RE_WORD.findall(self.text)
+    
+    def __getitem__(self, index: int) -> str:
+        return self.words[index]
+    
+    def __len__(self):
+        return len(self.words)
+    
+    def __repr__(self):
+        return f'Sentende({reprlib.repr(self.text)})'
+    
+    
