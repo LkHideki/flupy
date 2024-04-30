a7bd8393cf13eef5f7cc0c6b874955df1087d05c
LkHideki <lucas.hideki.07@gmail.com>
2024-04-30 16:28:12 -0300

    exemplo do spinner

---
+++ 05_fluxo/18_concorrencia_asyncio/01_intro2_spinner.py
@@ -0,0 +1,47 @@
+""" Implementando o exemplo do spinner
+"""
+
+import threading, itertools, time, sys
+
+
+class Signal:
+    go = True
+
+
+def spin(msg, signal):
+    write, flush = sys.stdout.write, sys.stdout.flush
+    ellipsis = itertools.cycle([".  ", ".  ", ".. ", ".. ", "...", "...", "..."])
+    for char in itertools.cycle("|/-\\"):
+        status = char + " " + msg + next(ellipsis)
+        write(status)
+        flush()
+        write("\x08" * len(status))
+        time.sleep(0.1)
+        if not signal.go:
+            break
+    write(" " * len(status) + "\x08" * len(status))
+
+
+def slow_function():
+    time.sleep(3)
+    return 42
+
+
+def supervisor():
+    signal = Signal()
+    spinner = threading.Thread(target=spin, args=("thinking", signal))
+    spinner.start()
+    result = slow_function()
+    print("spinner object:", spinner)
+    signal.go = False
+    spinner.join()
+    return result
+
+
+def main():
+    result = supervisor()
+    print("Answer:", result)
+
+
+if __name__ == "__main__":
+    main()
